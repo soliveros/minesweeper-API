0f39a5a6f7ca994e17cb5b250ed581dd7cf3fc13
Sebastián Oliveros <s.oliveros.cl@outlook.com>
2021-06-05 11:57:15 -0400

    Setting connection to mongo db using a configuration file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { GameModule } from './game/game.module';
+import {
+  Module
+} from '@nestjs/common';
+import {
+  UserModule
+} from './user/user.module';
+import {
+  GameModule
+} from './game/game.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from 'src/config/db.configuration';
 
 @Module({
-  imports: [UserModule, GameModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: false,
+      load: [
+        dbConfig,
+      ],
+    }),
+    UserModule, 
+    GameModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('database.host')}/${configService.get('database.name')}`,
+        useFindAndModify: false
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
