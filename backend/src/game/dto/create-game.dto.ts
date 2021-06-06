ff8991c42be8e2500ae7578c562a25d58dd1882b
Sebastián Oliveros <s.oliveros.cl@outlook.com>
2021-06-06 09:49:17 -0400

    Creating data transfer object for input validation and api documentation.

--- backend/src/game/dto/create-game.dto.ts
+++ backend/src/game/dto/create-game.dto.ts
@@ -1,1 +1,16 @@
-export class CreateGameDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateGameDto {
+    @ApiProperty({ example: 10, description: 'Number of rows for Minesweeper board.' })
+    rowsQuantity: number;
+
+    @ApiProperty({ example: 10, description: 'Number of columns for Minesweeper board.' })
+    columnsQuantity: number;
+
+    @ApiProperty({ example: 'soliveros', description: 'Username for player.' })
+    username: string;
+    
+    status: string;
+    hideBoard: number[][];
+    visibleBoard: boolean[][];    
+}
