2790f2fa1d1176a59ec61195ba7fef8526fccb04
Sebastián Oliveros <s.oliveros.cl@outlook.com>
2021-06-06 09:33:14 -0400

    Swagger implementation for REST API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  NestFactory
+} from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  AppModule
+} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Mine Sweeper REST API')
+    .setDescription('Rest API for Mine sweeper game.')
+    .setVersion('0.0.1')
+    .addTag('minesweeper-0.0.1')
+    .addBearerAuth()
+    .addSecurityRequirements("bearer")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8081);
 }
 bootstrap();
