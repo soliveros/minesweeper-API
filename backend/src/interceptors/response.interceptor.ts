d0c72c82433fa69bd212844a81d106e17e92fa56
Sebastián Oliveros <s.oliveros.cl@outlook.com>
2021-06-06 19:45:15 -0400

    Adding response interceptor to customize responses structure.

---
+++ backend/src/interceptors/response.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+} from '@nestjs/common';
+import {
+  Observable
+} from 'rxjs';
+import {
+  map
+} from 'rxjs/operators';
+
+export interface Response < T > {
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor < T > implements NestInterceptor < T, Response < T >> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable < Response < T >> {
+    return next.handle().pipe(map(result => ({
+      success: ((result.success != undefined) ? result.success : true),
+      message: result.message,
+      data: result.data
+    })));
+  }
+}
